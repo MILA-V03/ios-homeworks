826540e29a0b67597b98fb0a5653ad38dc3ce009
MILA-V03 <105431743+MILA-V03@users.noreply.github.com>
2022-06-30 16:45:26 +0300

    Add files via upload

---
+++ Netology_IB_Instruments2-master/Netology_IB_Instruments2/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments2
+//
+// 
+//
+
+import Foundation
+
+class ProfileViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
